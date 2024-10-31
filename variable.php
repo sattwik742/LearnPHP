9c7f26e5391bf46bca5c7157e2f4f3b76a1249e2
sattwik742 <sattwik211@gmail.com>
2024-10-31 15:50:16 +0000

    modify variable.php

--- variable.php
+++ variable.php
@@ -1,6 +1,28 @@
-// Creating php variables
+# Creating php variables
 
-In PHP, a variable starts with the $ sign, followed by the name of the variable:
+---------------------------------------------------------------------
 
+
+<?php
 $x = 5;
+$y = "Sattwik";
 
+echo $x;   # 5
+echo "br"; 
+echo $y;   # Sattwik
+?>
+
+# The variable $x will hold the value 5, and the variable $y will hold the value "John".
+
+
+# Rules for php variables----
+
+/*
+
+1. A variable starts with the $ sign, followed by the name of the variable.
+2. A variable name must start with a letter or the underscore character.
+3. A variable name cannot start with a number.
+4. A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ ).
+5. Variable names are case-sensitive ($age and $AGE are two different variables).
+
+*/
